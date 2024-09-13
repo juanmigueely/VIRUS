10bd53752dee582e72df93e3d3d698df81915a20
Juan Miguel Yepes <juanyepes0110z@gmail.com>
2024-09-13 10:01:15 -0500

    agregando

---
+++ js/micodigo.js
@@ -0,0 +1,14 @@
+
+//for (contador, comparacion, incremento)
+
+for (let i = 0; i < 5; i++){
+    console.log(i);
+}
+
+for (let contador = 9; contador < 40; contador++){
+    console.log("contador es ", contador, " el doble es ", contador + contador);
+}
+
+for (let j = 0; j < 5 ; j--){
+    console.log("jajajaja");
+}
